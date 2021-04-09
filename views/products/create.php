20d8f1b088f675c825f37efcd062a93b39eb90c8
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2021-04-09 10:40:17 +0200

    added back button

--- views/products/create.php
+++ views/products/create.php
@@ -1,4 +1,5 @@
-<header>
+<header class="d-flex justify-content-between align-items-center">
   <h1>Add a new product</h1>
+  <nav><a class="btn btn-secondary" href="/products">Back</a></nav>
 </header>
 <?php include_once('_form.php') ?>
